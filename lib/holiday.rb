8b0977ce2cd846da16d37eab7dfb67e0e8938b08
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 18:29:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,9 +74,14 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply| 
+      holiday if item.include?("BBQ")
+    end 
+  end.flatten.compact 
+  all_holidays_with_bbq(holiday_supplies)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
 
 
