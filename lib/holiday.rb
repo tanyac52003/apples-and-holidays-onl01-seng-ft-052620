8cf5edeefb198383d70248136e9be96af61c7e9d
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 18:23:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,6 +35,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].push(supply)
 end 
   add_supply_to_memorial_day(holiday_hash, supply)
+end 
 
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day arra
