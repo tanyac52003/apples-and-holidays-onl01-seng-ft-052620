709db7319cfa82ce1a841d3dac02d4283ecc9353
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 18:30:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply| 
-      holiday if item.include?("BBQ")
+      holiday if supply.include?("BBQ")
     end 
   end.flatten.compact 
   all_holidays_with_bbq(holiday_supplies)
